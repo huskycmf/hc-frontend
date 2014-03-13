1fba4e05c51d8af46b19e9c5d1c0d2024922087a
Oleksii Mylotskyi <oleksii.mylotskyi@milsdev.com>
2014-03-13 13:51:49 +0200

    Added language options to the config

--- config/module.config.php
+++ config/module.config.php
@@ -2,20 +2,11 @@
 return array(
     'router' => include __DIR__ . '/module/router.config.php',
 
-    'doctrine' => array(
-        'driver' => array(
-            'app_driver' => array(
-                'paths' => array(__DIR__ . '/../src/HcBackend/Entity')
-            ),
-            'orm_default' => array('drivers' => array('HcBackend\Entity' => 'app_driver'))
-        )
-    ),
-
     'service_manager' => include __DIR__ . '/module/service_manager.config.php',
     'di' => include __DIR__ . '/module/di.config.php',
 
     'hc-frontend'=> array(
-
+        'defaultLanguage' => 'en'
     ),
 
     'asset_manager' => array(
